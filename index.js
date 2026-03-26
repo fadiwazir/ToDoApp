a64fc4e528d59aed967ec0846c67c4f25d8538a6
FadiWazir <fadiwazirr@gmail.com>
2026-03-26 09:22:48 +0200

    stuck creating task

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world!")
+const addTask = document.getElementById("add-task");
+addTask.addEventListener("click", () => {
+  const inputText = document.getElementById("input-text").value;
+  const task = document.querySelector("task-container")
+  const createTask = document.createElement('li')
+  createTask.innerHTML = inputText
+  task.appendChild(createTask)
+})
+
