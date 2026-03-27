e2dd961f69d8575f6958b976b64d986a2b25a2fc
FadiWazir <fadiwazirr@gmail.com>
2026-03-27 08:40:26 +0200

    add task function

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const addTask = document.getElementById("add-task");
-addTask.addEventListener("click", () => {
+function addTask() {
   const inputText = document.getElementById("input-text").value;
-  const task = document.querySelector("task-container")
-  const createTask = document.createElement('li')
-  createTask.innerHTML = inputText
-  task.appendChild(createTask)
-})
+  const selectContainer = document.querySelector('ul')
+  selectContainer.innerHTML += `<li>${inputText}</li>`
+}
+
 
