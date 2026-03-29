cc07f78c6e7026eaa706c7aaf1c4c2efbdb0f8e3
FadiWazir <fadiwazirr@gmail.com>
2026-03-29 08:39:00 +0200

    delete button style

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+const taskArr = []
 function addTask() {
-  const inputText = document.getElementById("input-text").value;
-  const selectContainer = document.querySelector('ul')
-  selectContainer.hidden = false
-  selectContainer.innerHTML += `<li>${inputText}</li>`
+  let inputText = document.getElementById("input-text");
+  const selectContainer = document.querySelector('ul');
 
+  selectContainer.hidden = false
+  selectContainer.innerHTML += `
+  <div class="item">
+    <li>${inputText.value}</li>
+    <button class="delete-btn">Delete</button>
+  </div>`
+
+  inputText.value = ''
 }
+
+function enterBtn(){
+  inputText.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    console.log('is enter')
+  } else {
+    alert('Task Is empty')
+  }
+})
+}
+
 
 
